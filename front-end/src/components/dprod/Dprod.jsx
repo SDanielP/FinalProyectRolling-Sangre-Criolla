95344e15c7b4cf4b314a9ecfd880c20e77775dad
Joaquina Girbau <girbaujoaquina2@gmail.com>
2024-06-08 14:32:05 -0300

    Distribución dprod

--- front-end/src/components/dprod/Dprod.jsx
+++ front-end/src/components/dprod/Dprod.jsx
@@ -2,3 +2,23 @@ import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+
+function dProd() {
+    return (
+        <Container>
+          <Row xs={1} md={2}>
+            <Col> 
+            <p>Columna 1</p>
+            </Col>
+        <Col className='sm-col-12'> 
+          <p>Columna 2</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <p>Descripción</p>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
