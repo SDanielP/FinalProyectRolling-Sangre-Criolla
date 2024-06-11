1d0c56d1f1518b1b0fefeeb23cf6e64ffa5df222
Joaquina Girbau <girbaujoaquina2@gmail.com>
2024-06-11 13:12:52 -0300

    nombre de imagenes cambiadas, lógica de lectura de imagen carrousel hecha

--- front-end/src/components/shop/compProductDet/ImgP.jsx
+++ front-end/src/components/shop/compProductDet/ImgP.jsx
@@ -1,51 +1,58 @@
 import React from 'react';
 import Image from 'react-bootstrap/Image';
 
-function ImgP({id, category}) {
-    return ( 
-        <section >
-         <article id="carouselExample" className="carousel slide" data-bs-ride="carousel" data-bs-theme="dark">
-            <article className="carousel-inner">
-               <article className="carousel-item active">
-                  <Image className='border' src="https://www.hundeo.com/wp-content/uploads/2019/03/Shih-Tzu-Profil.jpg" fluid />
-               </article>
-               <article className="carousel-item">
-                  <Image className='border' src="https://www.purina.es/sites/default/files/styles/square_medium_440x440/public/2024-02/sitesdefaultfilesstylessquare_medium_440x440public2022-09Shih20Tzu.jpg?itok=wCQvw6qZ" fluid />
-               </article>
-               <article className="carousel-item">
-                  <Image className='border' src="https://www.purina.es/sites/default/files/2021-02/BREED%20Hero_0117_shih_tzu.jpg" fluid />
-               </article>
-               <article className="carousel-item">
-                  <Image className='border' src="https://artero.com/media/wysiwyg/Shih_Tzu2.png" fluid />
-               </article>
-            </article>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-               <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-               <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-               <span className="carousel-control-next-icon" aria-hidden="true"></span>
-               <span className="visually-hidden">Next</span>
-            </button>
-         </article>
-         <article className='container-fluid mt-3'>
-            <article className='row'>
-               <figure className='col-3'>
-               <Image className='col-12 cursor-pointer' src="https://www.hundeo.com/wp-content/uploads/2019/03/Shih-Tzu-Profil.jpg" data-bs-target="#carouselExample" data-bs-slide-to="0" aria-label="Slide 1" />
-               </figure>
-               <figure className='col-3'>
-               <Image  className=' col-12 cursor-pointer' src="https://www.purina.es/sites/default/files/styles/square_medium_440x440/public/2024-02/sitesdefaultfilesstylessquare_medium_440x440public2022-09Shih20Tzu.jpg?itok=wCQvw6qZ" data-bs-target="#carouselExample" data-bs-slide-to="1" aria-label="Slide 2" />
-               </figure>
-               <figure className='col-3'>
-               <Image className='col-12 cursor-pointer' src="https://www.purina.es/sites/default/files/2021-02/BREED%20Hero_0117_shih_tzu.jpg" data-bs-target="#carouselExample" data-bs-slide-to="2" aria-label="Slide 3" />
-               </figure>
-               <figure className='col-3'>
-               <Image className='col-12 cursor-pointer' src="https://artero.com/media/wysiwyg/Shih_Tzu2.png" data-bs-target="#carouselExample" data-bs-slide-to="3" aria-label="Slide 4" />
-               </figure>
-            </article>
-         </article>
-      </section>
-     );
-}
 
+function ImgP({ id }) {
+  // Obtener la ruta de la carpeta de imágenes del producto
+  const productImagesPath = `../../../assets/productsIMG/${id}`;
+
+  // Construir las rutas completas de las imágenes del producto
+  const productImages = Array.from({ length: 4 }, (_, index) => {
+    const imagePath = `${productImagesPath}/image${index + 1}.jpg`;
+    return index < 2 ? imagePath : null; // Retorna null para las rutas vacías
+  }).filter(image => image !== null); // Filtra las rutas vacías
+
+  // Obtener las rutas de las imágenes de la carpeta tallesSC
+  const tallesSCImages = Array.from({ length: 2 }, (_, index) => {
+    return `../../../assets/tallesSC/image${index + 1}.jpg`;
+  });
+
+  // Combinar las imágenes del producto y las de tallesSC
+  const combinedImages = [...productImages, ...tallesSCImages];
+
+  // Imprime las rutas de las imágenes en la consola
+
+  return (
+    <section>
+      <article id="carouselExample" className="carousel slide" data-bs-ride="carousel" data-bs-theme="dark">
+        <article className="carousel-inner">
+          {combinedImages.map((src, index) => (
+            <article key={index} className={`carousel-item ${index === 0 ? 'active' : ''}`}>
+              <Image className='border' src={src} fluid />
+            </article>
+          ))}
+        </article>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </article>
+      <article className='container-fluid mt-3'>
+        <article className='row'>
+          {combinedImages.map((src, index) => (
+            <figure key={index} className='col-3'>
+              <Image className='col-12 cursor-pointer' src={src} data-bs-target="#carouselExample" data-bs-slide-to={index} aria-label={`Slide ${index + 1}`} />
+            </figure>
+          ))}
+        </article>
+      </article>
+      <Image src="./asd.jpg" />
+
+    </section>
+  );
+}
 export default ImgP;
