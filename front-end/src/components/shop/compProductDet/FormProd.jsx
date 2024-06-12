8175fa4c48847136bd695d633a29d34977ab2cf8
Joaquina Girbau <girbaujoaquina2@gmail.com>
2024-06-12 08:46:40 -0300

    Arreglo Formulario de producto

--- front-end/src/components/shop/compProductDet/FormProd.jsx
+++ front-end/src/components/shop/compProductDet/FormProd.jsx
@@ -1,30 +1,29 @@
-import '../../../styles/components/shop/Dprod/Dprod.css';
+import '../../../styles/components/shop/Dprod/Dprod.css'
 import Button from 'react-bootstrap/Button';
-import FormuSize from '../compProductDet/FormS';
-import FormuColor from '../compProductDet/FormC';
+import FormSize from './FormS';
+import FormuColor from './FormC';
 
-function FormProd({id, category}) {
-    return (
-        <section className='formP container-fluid border mt-3 bg-opacity-10  p-2'>
-        <article className='ms-3'>
-        <h2 className='fs-3'>Nombre del producto</h2>
-        <h3 className='fs-5'>$$$$</h3>
-        </article>
-        <form className='mt-3 ms-3 me-3'>
-            <section className="mt-5 mb-3 d-grid gap-2">
-                <FormuColor />
-                <FormuSize />
-                <Button variant="outline-success btn-lg col-12" size="lg">
-                    Añadir al carrito
-                    </Button>
-                    <Button variant="outline-secondary" size="lg">
-                    Comprar ahora
-                    </Button>
-            </section>
-        </form>
+function FormProd({ name, price, color, sizes }) {
+  return (
+    <section className='formP container-fluid border mt-3 bg-opacity-10 p-2'>
+      <article className='ms-3'>
+        <h2 className='fs-3'>{name}</h2>
+        <h3 className='fs-4'>${price}</h3>
+        <h4 className='fs-5'>{color}</h4>
+      </article>
+      <form className='mt-3 ms-3 me-3'>
+        <section className=" mb-3 d-grid gap-2">
+          <FormSize sizes={sizes} />
+          <Button variant="outline-success btn-lg col-12" size="lg">
+            Añadir al carrito
+          </Button>
+          <Button variant="outline-secondary" size="lg">
+            Comprar ahora
+          </Button>
+        </section>
+      </form>
     </section>
-    );
-
+  );
 }
 
 export default FormProd;
