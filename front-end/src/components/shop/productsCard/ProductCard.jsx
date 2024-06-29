1467f21d38f995f377f291394ec8c5520bd7000b
AnaKuenneth <anakuenneth@hotmail.com>
2024-06-29 00:34:12 -0300

    Elimino la funcion de agregar al carrito rápidamente desde el ProductCard

--- front-end/src/components/shop/productsCard/ProductCard.jsx
+++ front-end/src/components/shop/productsCard/ProductCard.jsx
@@ -1,6 +1,6 @@
 import "../../../styles/components/shop/productsCard/ProductCard.css";
 import "../../../styles/components/shop/Products.css";
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -8,22 +8,13 @@ import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import { faBagShopping } from "@fortawesome/free-solid-svg-icons";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import Tooltip from "react-bootstrap/Tooltip";
-import Popover from "react-bootstrap/Popover";
 import fetchProductById from "../compProductDet/fetchProductById";
-
-/* ----- Importaciones de Silvio ----- */
-// ProductCard.jsx
-// import { useState } from 'react';
-// import Card from 'react-bootstrap/Card';
-// import ListGroup from 'react-bootstrap/ListGroup';
-// import Button from 'react-bootstrap/Button';
-// import { useNavigate } from 'react-router-dom';
-import { Form, FormGroup, Input } from 'reactstrap';
-// import './ProductCard.css';
-import useCart from '../carts/useCart';
+import useCart from "../carts/useCart";
 
 const ProductCard = ({ producto }) => {
   const navigate = useNavigate();
+  const [cartProducts, updateCart] = useCart();
+  const [tooltipText, setTooltipText] = useState('Añadir al carrito');
 
   const handleQuickView = async () => {
     try {
@@ -34,84 +25,46 @@ const ProductCard = ({ producto }) => {
     }
   };
 
-    /* ----- Mensajes durante hover en botones Carrito y Fav -----*/
-    const tooltipAddToCart = (props) => (
-      <Tooltip id="tooltip" {...props} className="hoverBag">
-        Añadir al carrito
-      </Tooltip>
-    );
+  const tooltipAddToCart = (props) => (
+    <Tooltip id="tooltip" {...props} className="hoverBag">
+      {tooltipText}
+    </Tooltip>
+  );
 
-    const tooltipAddToFavs = (props) => (
-      <Tooltip id="tooltip" {...props} className="hoverHeart">
-        Añadir a la lista de favoritos
-      </Tooltip>
-    );
+  const tooltipAddToFavs = (props) => (
+    <Tooltip id="tooltip" {...props} className="hoverHeart">
+      Añadir a la lista de favoritos
+    </Tooltip>
+  );
 
-    /* ----- Estados para botones de Agregar al Carrito y Favoritos dento de cada card*/
-    const [mostrarOpciones, setMostrarOpciones] = useState(false);
-
-    const mostrarOpcionesCarrito = () => {
-      setMostrarOpciones(!mostrarOpciones);
-    };
-
-    /* ----- Funcionalidad AGREGAR AL CARRITO de Silvio ----- */
-
-    const [quantities, setQuantities] = useState({ [producto.id]: 1 });
-    const [cartProducts, updateCart] = useCart();
-
-    const [buttonText, setButtonText] = useState('Añadir al Carrito');
-    // const [buttonClass, setButtonClass] = useState('');
-
-    const handleQuantityChange = (e) => {
-      const { id, value } = e.target;
-      setQuantities({ ...quantities, [id]: value });
-    };
-  
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
+  const handleAddToCart = () => {
     const updatedCart = [...cartProducts];
-
-    for (const [id, quantity] of Object.entries(quantities)) {
-      if (parseInt(quantity) > 0) {
-        const existingProductIndex = updatedCart.findIndex((product) => product.id === id);
-        if (existingProductIndex !== -1) {
-          updatedCart[existingProductIndex].quantity = parseInt(quantity);
-        } else {
-          const newProduct = {
-            id,
-            name: producto.name,
-            quantity: parseInt(quantity),
-            price: producto.price
-          };
-          updatedCart.push(newProduct);
-        }
-      }
+    const existingProductIndex = updatedCart.findIndex((product) => product.id === producto._id);
+    
+    if (existingProductIndex !== -1) {
+      updatedCart[existingProductIndex].quantity += 1;
+    } else {
+      const newProduct = {
+        id: producto._id,
+        name: producto.name,
+        quantity: 1,
+        price: producto.price
+      };
+      updatedCart.push(newProduct);
     }
 
-    // Cambiar el texto del botón y agregar la clase de animación
-    setButtonText('Producto añadido');
-    // setButtonClass('added');
-
-    // Restablecer el texto y la clase del botón después de 2 segundos
+    setTooltipText('Producto añadido');
     setTimeout(() => {
-      setButtonText('Añadir al Carrito');
-      // setButtonClass('');
+      setTooltipText('Añadir al carrito');
     }, 2000);
 
     updateCart(updatedCart);
-    // alert('Productos añadidos al carrito');
-    setQuantities({ [producto._id]: 1 });
   };
 
   return (
     <div className="background-body">
       <div className="containerCard" key={producto._id}>
-        {/* --- Imagen producto y botones (carrito, favorito y vista rapida/añadir al carrito) ---*/}
         <div className="card__container tarjetaProducto">
-
-          {/* --- Imagen producto ---*/}
           <article className="contenedorImg">
             <img
               src={producto.image}
@@ -119,48 +72,16 @@ const ProductCard = ({ producto }) => {
               className="card__img imgProducto"
               onClick={handleQuickView}
             ></img>
-
-            {/* --- Botones (vista rapida/añadir al carrito) --- */}
-            {mostrarOpciones === false ? (
-              <div className="card__data">
-                <Button
-                  className="card__button__vistaRapida"
-                  onClick={handleQuickView}
-                >
-                  Vista rápida
-                </Button>
-              </div>
-            ) : (
-              <div className="card__data">
-                <div id="transition">
-                  <Form onSubmit={handleSubmit} className="formCart">
-                    <FormGroup className="quantity">
-                      <Input
-                        id={producto._id}
-                        type="number"
-                        placeholder="Cantidad"
-                        value={quantities[producto._id]}
-                        onChange={handleQuantityChange}
-                      />
-                    </FormGroup>
-                    
-
-                     <Button
-                       id="addToCart"
-                       color="primary"
-                       type="submit"
-                       className="card__button__añadirCarrito"
-                     >
-                       {buttonText}
-                     </Button> 
-                    
-                  </Form>
-                </div>
-              </div>
-            )}
+            <div className="card__data">
+              <Button
+                className="card__button__vistaRapida"
+                onClick={handleQuickView}
+              >
+                Vista rápida
+              </Button>
+            </div>
           </article>
 
-          {/* --- Botones (carrito, favorito) --- */}
           <OverlayTrigger
             placement="bottom"
             overlay={tooltipAddToFavs}
@@ -177,17 +98,16 @@ const ProductCard = ({ producto }) => {
               icon={faBagShopping}
               size="xl"
               className="bag-btn"
-              onClick={mostrarOpcionesCarrito}
+              onClick={handleAddToCart}
             />
           </OverlayTrigger>
         </div>
 
-        {/* --- Nombre del producto y precio --- */}
         <div className="card-body">
           <Button
             variant="outline-dark"
             className="btnVerProducto"
-            onClick={handleQuickView} // Redirigir a la página del producto
+            onClick={handleQuickView}
             style={{ height: "6rem", objectFit: "contain", fontSize: "0.9rem" }}
           >
             {producto.name}
