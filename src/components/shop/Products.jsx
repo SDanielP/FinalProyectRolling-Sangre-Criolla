a1e760fd411cdb9aad52b8ff35267649917dbb30
AnaKuenneth <anakuenneth@hotmail.com>
2024-07-20 23:09:38 -0300

    Paginación en la tienda implementada

--- src/components/shop/Products.jsx
+++ src/components/shop/Products.jsx
@@ -5,6 +5,7 @@ import { useProducts } from "../../store/useProducts";
 import { useParams } from "react-router-dom";
 import Loader from "./apiMessages/Loader";
 import Ups from "./apiMessages/Ups";
+import Button from 'react-bootstrap/Button';
 
 const categoriesOptions = [
   { id: 1, value: "mujeres", label: "Mujeres" },
@@ -14,18 +15,17 @@ const categoriesOptions = [
 //***URL API
 const url = "https://sangrecriolla-back-end.onrender.com";
 
-const Products = ({
-  ordenar,
-  precioMin,
-  precioMax,
-  tamano,
-  color,
-}) => {
+const Products = ({ ordenar, precioMin, precioMax, tamano, color }) => {
   const { products, setProducts } = useProducts();
   const [productosStore, setProductosStore] = useState([]);
   const { category, subcategory } = useParams();
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+
+  /*-----Para la paginación */
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsPerPage, setProductsPerPage] = useState(15);
+  /*----- */
 
   const getProductos = useCallback(async () => {
     setLoading(true);
@@ -114,8 +114,40 @@ const Products = ({
     color,
   ]);
 
+  /*----Lógica Paginación */
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setProductsPerPage(10); // Mobile: 5 filas x 2 productos
+      } else if (window.innerWidth < 1024) {
+        setProductsPerPage(12); // Tablet: 4 filas x 3 productos
+      } else {
+        setProductsPerPage(15); // Web: 3 filas x 5 productos
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Initial check
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const totalPages = Math.ceil(productosStore.length / productsPerPage);
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = productosStore.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+    window.scrollTo(0, 0); // Scroll to top
+  }
+
+  /* -----*/
+
   if (loading) {
-    return( <Loader />);
+    return <Loader />;
   }
 
   if (error) {
@@ -125,11 +157,27 @@ const Products = ({
   return (
     <div>
       {productosStore.length > 0 ? (
-        <div className="contenedorProductos" key={1}>
-          {productosStore.map((producto) => (
-            <ProductCard key={producto._id} producto={producto} />
-          ))}
-        </div>
+        <>
+          {/* Productos */}
+          <div className="contenedorProductos" key={1}>
+            {currentProducts.map((producto) => (
+              <ProductCard key={producto._id} producto={producto} />
+            ))}
+          </div>
+          
+          {/* Barra de paginación */}
+          <div className="pagination">
+            {Array.from({ length: totalPages }, (_, i) => (
+              <Button variant="dark"
+                key={i + 1}
+                onClick={() => paginate(i + 1)}
+                className={currentPage === i + 1 ? "active" : ""}
+              >
+                {i + 1}
+              </Button>
+            ))}
+          </div>
+        </>
       ) : (
         <Ups />
       )}
