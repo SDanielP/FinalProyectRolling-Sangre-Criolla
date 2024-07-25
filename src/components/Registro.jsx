38de631049c01a929847567ec993ec07a953b7a3
Maria Kuenneth <mariakuenneth@gmail.com>
2024-07-25 07:47:29 -0300

    Agregado de notificaciones de registro. Limpieza de formulario incompleta--> REVISAR

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react';
+import React,{ useState } from 'react';
 import "../styles/styleInicioSesion.css"
+import toast, {Toaster}  from 'react-hot-toast';
+import {useRef} from 'react';
 const url = "https://sangrecriolla-back-end.onrender.com";
 
 const Registro = ({toggleComponent}) => {
+    
     const [formData, setFormData] = useState({
         name: '',
         email: '',
         password: ''
     });
+
+    const form = useRef(null);
 
     // Handler para el cambio de los campos del formulario
     const handleInputChange = (e) => {
@@ -16,9 +21,9 @@ const Registro = ({toggleComponent}) => {
     };
 
     // Handler para el envío del formulario de registro
+
     const handleRegisterSubmit = async (e) => {
-        e.preventDefault();
-        
+        e.preventDefault();  
         try {
             const response = await fetch(`https://sangrecriolla-back-end.onrender.com/api/auth/register`, {
                 method: 'POST',
@@ -29,11 +34,16 @@ const Registro = ({toggleComponent}) => {
             });
             if (response.ok) {
                 console.log('Usuario registrado con éxito.');
+                toast.success('Usuario registrado con éxito.');
+                // form.current.reset()
+                e.target.reset();
             } else {
                 console.error('Error al registrar el usuario:', response.statusText);
+                toast.error('Error al registrar el usuario:');
             }
         } catch (error) {
             console.error('Error al registrar el usuario:', error);
+            toast('Error al registrar el usuario:');
         }
     };
     return (
@@ -58,6 +68,7 @@ const Registro = ({toggleComponent}) => {
                     {/* --- INPUT DE CONFIRMAR CONTRASEÑA --- */}
                     {/* <input className="input-iniciarSesion transparente-iniciarSesion" type="password" autoComplete="off" id='confirmContraseña' name= 'confirmPassword' placeholder="Confirmar contraseña" required /><br /> */}
                     <button type="submit" className='submit-iniciarSesion'>Registrarse</button>
+                    <Toaster/>
                 </form>
             </div>
         </div>
